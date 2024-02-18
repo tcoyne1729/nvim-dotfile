bc20e059fdd7011fc0f0e81d22c011732ccda3ea
Tom Coyne <tcoyne1729@gmail.com>
2024-02-18 19:44:05 +0000

    add molten
    add the requirements to run jupyter cells

--- lua/tom/core/basic-conf.lua
+++ lua/tom/core/basic-conf.lua
@@ -2,3 +2,4 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.signcolumn = "number"
+vim.g.python3_host_prog = "/home/tomcoyne/.local/share/virtualenvs/nvim-python-XCcB9VJN/bin/python"
